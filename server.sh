ff4b864ffb4d17420b1e47eced3d787210246994
Nathazha27 <nathazha.bernales@enti.cat>
2025-06-11 20:35:03 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -25,7 +25,7 @@ echo "5. RECIBO FILE_NAME"
 
 PREFIX=`echo "$DATA" | cut -d " " -f 1`
 FILE_NAME=`echo "$DATA" | cut -d " " -f 2`
-FILE_CLIENT_HASH=`echo "$DATA" | cut -d " " -f 3`
+FILE_NAME_MD5=`echo "$DATA" | cut -d " " -f 3`
 
 if [ "$PREFIX" != "FILE_NAME" ]
 then
@@ -36,10 +36,10 @@ fi
 
 FILE_HASH=$(echo -n "$FILE_NAME" | md5sum)
 
-if [ "$FILE_CLIENT_HASH" != "$FILE_HASH" ]
+if [ "$FILE_NAME_MD5" != "$FILE_HASH" ]
 then
 echo "ERROR 3: Hash incorrecto"
-echo "KO_FILE_HASH" | nc localhost $PORT
+echo "KO_FILE_MD5" | nc localhost $PORT
 exit 3
 fi
 
@@ -52,20 +52,36 @@ CONTENIDO=`nc -l $PORT`
 
 echo $CONTENIDO > server/$FILE_NAME
 
-echo "OK_DATA" | nc localhost $PORT
-
-DATA=`nc -l $PORT`
-HASH=`cat client/dragon.txt | md5sum`
-
-echo "8. COMPRUEBA SI ESTÁ VACÍO"
-
-echo "11. RECIBE Y COMPRUEBA MD5"
-
-if [ "$DATA" != "$HASH" ]
+echo "8. Comprobamso si el archivo está vacío"
+if [ ! -s server/$FILE_NAME ]
 then
-echo "ERROR 4: Contenido incorrecto"
-echo "KO_MD5" | nc localhost $PORT
+echo "ERROR 4: Archivo vacío"
+echo "KO_DATA" | nc localhost $PORT
 exit 4
 fi
 
-echo "OK_MD5" | nc localhost $PORT
+echo "OK_DATA" | nc localhost $PORT
+
+DATA=`nc -l $PORT`
+
+Prefix_MD5=`echo -n "$DATA" | cut -d " " -f 1`
+FILE_MD5=`echo -n "$DATA" | cut -d " " -f 2`
+HASH=`cat client/dragon.txt | md5sum`
+
+echo "11. RECIBE Y COMPRUEBA PREFIJO Y MD5"
+
+if [ "$Prefix_MD5" != "FILE_MD5" ]
+then
+echo "ERROR 5: Prefijo incorrecto"
+echo "KO_FILE_MD5" | nc localhost $PORT
+exit 5
+fi
+
+if [ "$FILE_MD5" != "$HASH" ]
+then
+echo "ERROR 6: Contenido incorrecto"
+echo "KO_FILE_MD5" | nc localhost $PORT
+exit 6
+fi
+
+echo "OK_FILE_MD5" | nc localhost $PORT
